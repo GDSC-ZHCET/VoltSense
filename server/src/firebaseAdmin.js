8d172ef74491d172266b82ebf3223b215b7ab5d5
Avyukt Soni <soniavyukt@gmail.com>
2025-02-27 21:56:37 +0530

    updated server CI/CD

--- server/src/firebaseAdmin.js
+++ server/src/firebaseAdmin.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
 require("dotenv").config();
+const fs = require("fs");
 
 if (!admin.apps.length) {
   admin.initializeApp({
@@ -7,6 +8,10 @@ if (!admin.apps.length) {
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Fix newline issue
+      // privateKey: fs.readFileSync(
+      //   process.env.FIREBASE_PRIVATE_KEY_FILE,
+      //   "utf8"
+      // ),
     }),
   });
 }
