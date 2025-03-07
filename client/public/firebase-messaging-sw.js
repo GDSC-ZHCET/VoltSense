bae8b70c780bfbb8d0ed354fe4b32ccf8316d817
Avyukt Soni <soniavyukt@gmail.com>
2025-03-07 10:44:09 +0530

    changes in Dockerfile

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,36 +1,3 @@
-// self.addEventListener("message", (event) => {
-//   if (event.data && event.data.type === "INIT_FIREBASE") {
-//     const firebaseConfig = event.data.firebaseConfig;
-
-//     // Import Firebase scripts
-//     importScripts(
-//       "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
-//     );
-//     importScripts(
-//       "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js"
-//     );
-
-//     firebase.initializeApp(firebaseConfig);
-
-//     const messaging = firebase.messaging();
-
-//     messaging.onBackgroundMessage((payload) => {
-//       console.log("Received background message:", payload);
-
-//       const notificationTitle = payload.notification.title;
-//       const notificationOptions = {
-//         body: payload.notification.body,
-//         icon: "/voltsense.png",
-//       };
-
-//       self.registration.showNotification(
-//         notificationTitle,
-//         notificationOptions
-//       );
-//     });
-//   }
-// });
-
 // Import Firebase scripts first
 importScripts(
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
